bc8ab7f582cb04876636dc8653fe4f12866a48ef
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-03-08 12:40:13 +0530

    Create DS3.py

---
+++ EEI-3372-Python/DS3.py
@@ -0,0 +1,40 @@
+# def change_string(str):
+#   str = str + " How are you"
+#   print("printing the string inside function:", str)
+
+# string1 = "Hi I am there"
+
+# change_string(string1)
+# print("printing the string outside function:", string1)
+
+
+# def findMax(num1, num2):
+#   if (num1 > num2):
+#     return num1
+#   else: 
+#     return num2
+  
+
+# print("The max number is:",findMax(12, 4))
+
+ 
+
+x = 1
+
+y = 2
+
+z = 3
+
+ 
+
+ 
+
+def fun(x , y = 7, z=0):
+
+    print(x,y,z)
+
+    
+
+print(fun(10,20,30))
+
+print(fun(10,30))
