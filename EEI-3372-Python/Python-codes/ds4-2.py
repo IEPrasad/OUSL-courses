b54064c610d7585c60626c6c5137654771a98ce1
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-03-27 16:38:07 +0530

    Add files via upload

---
+++ EEI-3372-Python/Python-codes/ds4-2.py
@@ -0,0 +1,12 @@
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+# use the person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
