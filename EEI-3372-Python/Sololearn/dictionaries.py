1deb6db5876ad12545d94dbd7e858794ef9427fd
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-05-06 23:39:20 +0530

    Update dictionaries.py

--- EEI-3372-Python/Sololearn/dictionaries.py
+++ EEI-3372-Python/Sololearn/dictionaries.py
@@ -23,3 +23,26 @@ uppercase_words = [word.upper() for word in words]
 print(uppercase_words)
 
 
+
+    # 4 Creating a list of tuples containing numbers and their squares:
+
+numbers = [1, 2, 3, 4, 5]
+number_square_pairs = [(x, x ** 2) for x in numbers]
+print(number_square_pairs)
+
+
+
+    # 5 Flattening a 2D list:
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flattened_list = [item for sublist in matrix for item in sublist]
+print(flattened_list)
+
+
+    # Creating a dictionary from two lists:
+
+keys = ['a', 'b', 'c']
+values = [1, 2, 3]
+dictionary = {k: v for k, v in zip(keys, values)}
+print(dictionary)
+
