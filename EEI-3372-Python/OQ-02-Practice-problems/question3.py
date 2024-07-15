2f19b770e3e48c35ded7a8461f2dced534bbedfe
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-07-15 22:59:30 +0530

    Create question3.py

---
+++ EEI-3372-Python/OQ-02-Practice-problems/question3.py
@@ -0,0 +1,10 @@
+Functions 
+
+Question: Write a function add_numbers that takes two numbers as arguments and returns their sum. Call this function with the 
+arguments 5 and 10, and print the result.
+
+def add_numbers(a, b):
+    return a + b
+
+result = add_numbers(5, 10)
+print(result)  # Output: 15
