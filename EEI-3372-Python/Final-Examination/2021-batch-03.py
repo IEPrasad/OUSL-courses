d07a0728bf39f00b9e2ba1ba0cf84d731bb05151
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-08-03 19:26:09 +0530

    Create 2021-batch-03.py

---
+++ EEI-3372-Python/Final-Examination/2021-batch-03.py
@@ -0,0 +1,25 @@
+# 1) c)
+# c. Write a simple snippet for the following questions
+# Consider the lists, A = [77,9,”u”,3,5] B = [ 1,”y”,”s”]
+# (i) Add numbers 12 and the letter “u” to the list A. [3 marks]
+# (ii) Remove number 9 from the list A. [3 marks]
+# (iii) Remove all the items from list B. 
+
+# i)
+A = [77, 9, "u", 3, 5]
+B = [1, "y", "s"]
+
+A.append(12)
+A.append("u")
+print(A)
+
+# ii)
+A.remove(9)
+print(A)
+
+# iii)
+B.clear()
+print(B)
+
+
+
