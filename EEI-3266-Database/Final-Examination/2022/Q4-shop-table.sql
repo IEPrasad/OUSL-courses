9f104532e7a713d8c768eed8650c206c5c0159f3
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-08-16 23:42:01 +0530

    Update Q4-shop-table.sql

--- EEI-3266-Database/Final-Examination/2022/Q4-shop-table.sql
+++ EEI-3266-Database/Final-Examination/2022/Q4-shop-table.sql
@@ -1,17 +1,15 @@
 CREATE DATABASE Details;
 USE Details;
+
 CREATE TABLE shops (
   Shop_Id varchar(10) PRIMARY KEY,
   Shop_Name varchar(50),
   Area varchar(30)
 );
 
-INSERT DATA shops (Shop_Id, Shop_Name, Area) VALUES 
+INSERT INTO shops (Shop_Id, Shop_Name, Area) VALUES 
   ("S001", "ABC Computers", "CP"),
   ("S002", "All Infotech Media", "GK II"),
   ("S003", "Tech Shope", "CP"),
   ("S004", "Geek Techno Soft", "Ward Place"),
   ("S005", "Hitech Tech Store", "Ward Place");
-
-
-  
