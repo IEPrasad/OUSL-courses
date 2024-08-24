36326c4f89ac3f7afe4251860f5ff769f330688b
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-08-25 01:14:29 +0530

    Update PE-problem3.py

--- EEI-3372-Python/PE-problem3.py
+++ EEI-3372-Python/PE-problem3.py
@@ -3,16 +3,21 @@
 #     What is the largest prime factor of the number 600851475143?
 
 x = 600851475143
-i = 1
+i = 600851475143
 result = 0
 
-while (i <= x):
+while (i >= 1):
     result = x/i
-    while (result >= 1):
-        print(result)
-        i += 1
-    i += 1
-# i = 10
-# while (i >= 0):
-#     print(i)
-#     i -= 1
+    if (i >= 1):
+        if (result <= x and x % i == 0 and i % 2 != 0):
+            print(i)
+            i -= 1
+    else:
+        break
+    i -= 1
+    
+
+
+
+#  this version seem like correct and 
+# try this code again
