73abe1cd33bf6a761831810a253e3843982605df
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-08-30 20:47:36 +0530

    Create switch-statement.c

---
+++ EEX3467 - Software Engineering Concepts and Programming/switch-statement.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+  int num = 1;
+  switch (num) {
+    case 1: 
+      printf("Monday\n");
+      break;
+
+    case 2: 
+      printf("Tuesday\n");
+      break;
+
+    case 3:
+      printf("Wednesday\n");
+      break;
+
+    case 4:
+      printf("Thursday\n");
+      break;
+    
+    case 5:
+      prinf("Friday\n");
+      break;
+
+    default:  
+      printf("Weekend\n");
+      
+  }
+    
+}  
