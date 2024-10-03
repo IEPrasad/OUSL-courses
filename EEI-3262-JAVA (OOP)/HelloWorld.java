a76a91004216829f6685e9f8fb293d340743b557
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-10-04 00:00:43 +0530

    Create HelloWorld.java

---
+++ EEI-3262-JAVA (OOP)/HelloWorld.java
@@ -0,0 +1,7 @@
+// This is simple program that prints "Hello, World!"
+public class HelloWorld {
+  public static void main(String[] args) {
+    // Prints the message to the console
+    System.out.println("Hello, World");
+  }
+}
