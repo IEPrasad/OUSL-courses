9c865e065a150e584e616ee57fad3aed8283001b
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-10-09 23:25:50 +0530

    Create ps2-1.php
    how to use some string functions in PHP

---
+++ EEI-4346-Web-Technology/PHP/PS_2/ps2-1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>This is the first file with the xampp server</h2>
+
+    <?php 
+
+        echo "<br/>";
+        echo "String Functions";
+        echo "<br/>";
+        echo "<br/>";
+
+        echo strlen("Programming");
+
+        echo "<br/>";
+
+        echo str_word_count("The open university of Sri Lanka");
+
+    ?>
+</body>
+</html>
