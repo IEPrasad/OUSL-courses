82bebec5c7a95b33429ffa962153a85c8684dc79
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-10-19 23:55:14 +0530

    Create index.php

---
+++ EEI-4346-Web-Technology/PHP/CAT2/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php 
+  // Check if 'name' parameter is set in the URL
+  if (isset($_GET['name'] && !empty($_GET['name']))) {
+    $_name = htmlspecialchars($_GET['name']); // Sanitize the input
+    echo "<h2>Hello, $name!</h2>";
+  }
+?>
+
+<form action="#" method="GET">
+  <label>
+    Enter your name:
+    <input type="text" name="name">
+    <input type="submit" value="SUBMIT">
+  </label>
+</form>
+</body>
+</html>
