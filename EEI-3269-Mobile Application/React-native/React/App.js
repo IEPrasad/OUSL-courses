2f5359ede4d3188ac152be808b1d15f075ed801c
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-10-24 23:18:59 +0530

    Cr

---
+++ EEI-3269-Mobile Application/React-native/React/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        {/* Button added here */}
+        <button className='App-button' onClick={() => alert("Button clicked!")}>
+          Click Me
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
