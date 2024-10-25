b0c64d22623f2411cba3fa42080310f7f3ddd760
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-10-25 23:54:50 +0530

    inputs in the react

---
+++ EEI-3269-Mobile Application/React-native/React/input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Input.css';
+
+function Input() {
+  return (
+    <input
+      type="text"
+      className="custom-input"
+      placeholder="Enter text here"
+    />
+  );
+}
+
+export default Input;
