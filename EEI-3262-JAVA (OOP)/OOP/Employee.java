dd3f357d9aeff214847024842eaa67398adb868a
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-11-02 19:22:24 +0530

    This code also have some errors

--- EEI-3262-JAVA (OOP)/OOP/Employee.java
+++ EEI-3262-JAVA (OOP)/OOP/Employee.java
@@ -1,38 +1,87 @@
-class Employee{
+class Employee {
   private String name;
   private int age;
   private boolean married;
-
   private double salary;
   private String department;
-}
 
-public String getName() {
-  return name;
-}
+  // Constructor
+  public Employee(String name, int age, boolean married) {
+    this.name = name;
+    this.age = age;
+    this.married = married;
+  }
 
-public boolean isMarried() {
-  return married;
-}
+  // Getter for name
+  public String getName() {
+    return name;
+  }
 
-public double getSalary() {
-  return salary;
-}
+  // Getter for age
+  public int getAge() {
+    return age;
+  }
 
-public void setSalary (double newSalary) {
-  salary = newSalary;
-}
+  // Getter for married status
+  public boolean isMarried() {
+    return married;
+  }
 
-public String getDepartment () {
-  return department;
-}
+  // Getter and setter for salary
+  public double getSalary() {
+    return salary;
+  }
+class Employee {
+  private String name;
+  private int age;
+  private boolean married;
+  private double salary;
+  private String department;
 
-public void setDepartment (String newDepartment) {
-  department = newDepartment;
-}
+  // Constructor
+  public Employee(String name, int age, boolean married) {
+    this.name = name;
+    this.age = age;
+    this.married = married;
+  }
 
-public void doWork (String workToPerform) {
-  System.out.println("I am doing :" + workToPerform);
+  // Getter for name
+  public String getName() {
+    return name;
+  }
+
+  // Getter for age
+  public int getAge() {
+    return age;
+  }
+
+  // Getter for married status
+  public boolean isMarried() {
+    return married;
+  }
+
+  // Getter and setter for salary
+  public double getSalary() {
+    return salary;
+  }
+
+  public void setSalary(double newSalary) {
+    salary = newSalary;
+  }
+
+  // Getter and setter for department
+  public String getDepartment() {
+    return department;
+  }
+
+  public void setDepartment(String newDepartment) {
+    department = newDepartment;
+  }
+
+  // Method to simulate work
+  public void doWork(String workToPerform) {
+    System.out.println("I am doing: " + workToPerform);
+  }
 }
 
 class Company {
@@ -40,20 +89,54 @@ class Company {
     Employee emp1 = new Employee("Nipun", 35, true);
     Employee emp2 = new Employee("Shani", 25, false);
   
-    // interactig with new Employee emp1
+    // Interacting with new Employee emp1
     System.out.println(emp1.getName());
     System.out.println(emp1.getAge());
     System.out.println(emp1.isMarried());
-
-
     emp1.doWork("Arrange the Files");
 
-    // interactig with new Employee emp2 
+    // Interacting with new Employee emp2 
     System.out.println(emp2.getName());
     System.out.println(emp2.getAge());
     System.out.println(emp2.isMarried());
-
     emp2.doWork("Answer the calls");
+  }
+}
+
+  public void setSalary(double newSalary) {
+    salary = newSalary;
+  }
+
+  // Getter and setter for department
+  public String getDepartment() {
+    return department;
+  }
+
+  public void setDepartment(String newDepartment) {
+    department = newDepartment;
+  }
+
+  // Method to simulate work
+  public void doWork(String workToPerform) {
+    System.out.println("I am doing: " + workToPerform);
+  }
+}
+
+class Company {
+  public static void main(String[] args) {
+    Employee emp1 = new Employee("Nipun", 35, true);
+    Employee emp2 = new Employee("Shani", 25, false);
   
+    // Interacting with new Employee emp1
+    System.out.println(emp1.getName());
+    System.out.println(emp1.getAge());
+    System.out.println(emp1.isMarried());
+    emp1.doWork("Arrange the Files");
+
+    // Interacting with new Employee emp2 
+    System.out.println(emp2.getName());
+    System.out.println(emp2.getAge());
+    System.out.println(emp2.isMarried());
+    emp2.doWork("Answer the calls");
   }
 }
