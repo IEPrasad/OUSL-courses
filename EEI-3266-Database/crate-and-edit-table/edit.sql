87aae3238758552ab7e1e05510041ba2558e8a00
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-11-20 23:47:56 +0530

    Create edit.sql

---
+++ EEI-3266-Database/crate-and-edit-table/edit.sql
@@ -0,0 +1,8 @@
+-- Update data in Students table
+UPDATE Students
+SET Major = 'Computer Science'
+WHERE StudentID = 1;
+
+-- Delete a row from Courses table
+DELETE FROM Courses
+WHERE CourseID = 2;
