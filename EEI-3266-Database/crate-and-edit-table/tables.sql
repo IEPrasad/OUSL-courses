33f2a9abc47b74e4e3b86c75585c150f0fff80ec
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-11-20 23:47:36 +0530

    Create tables.sql

---
+++ EEI-3266-Database/crate-and-edit-table/tables.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    Age INT,
+    Major VARCHAR(50)
+);
+
+CREATE TABLE Courses (
+    CourseID INT PRIMARY KEY,
+    CourseName VARCHAR(100),
+    Credits INT,
+    Instructor VARCHAR(100)
+);
