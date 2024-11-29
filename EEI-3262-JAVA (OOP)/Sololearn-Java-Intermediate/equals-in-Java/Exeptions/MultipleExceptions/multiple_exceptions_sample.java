63437e34254830ec634ac73db14152fc6dbb4364
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2024-11-29 23:56:45 +0530

    Updated

--- EEI-3262-JAVA (OOP)/Sololearn-Java-Intermediate/equals-in-Java/Exeptions/MultipleExceptions/multiple_exceptions_sample.java
+++ EEI-3262-JAVA (OOP)/Sololearn-Java-Intermediate/equals-in-Java/Exeptions/MultipleExceptions/multiple_exceptions_sample.java
@@ -18,3 +18,16 @@ public class multiple_exceptions_sample {
   The <b> throw </b> keyword allows you to manually generate exceptions from your methods. Some of the numerous available exception types include the indexOutOfBoundsException, IllegalArguementException, ArithmaticException, and so on.
   For example, we can throw an ArithmaticException in our method when the parameter is 0.
 */ 
+
+
+/*
+  The <b>throws </b>statement in the method definition defines the type of Exception(s) the method can throw.
+
+  Next, the <b>throw </b>keyword throws the corresponding exception, along with a custom message.
+
+  If we call the div method with the second parameter equal to 0, it will throw an ArithmeticException with the message "Division by Zero".
+*/
+
+/*
+  Multiple exceptions can be defined in the throws statement using a comma-separated list.
+*/
