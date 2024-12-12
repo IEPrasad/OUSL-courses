9dc80464cd87e3516200489ca2e1b5e2400c9847
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-12-12 23:44:56 +0530

    Introduction of the OOP

---
+++ EEI-3262-JAVA (OOP)/OOP/Intro.java
@@ -0,0 +1,13 @@
+1. What is OOP?
+  Object-Oriented Programming (OOP) is a programming paradigm based on the concept of "objects", which
+  can contain ata in the form of fields (often known as attributes or properties) and code 
+  in the form of methods (functions). OOP helps to organize software design in a more modular
+  and reusable way.
+
+
+2. Key Concepts of OOP
+  There are four main pillars of Object-Oriente Programming:
+  1. Encapsulation 
+  2. Abstraction 
+  3. Inheritance
+  4. Polymorphism
