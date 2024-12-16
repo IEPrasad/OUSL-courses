597f326652df25c67a0372dafa69c53f1324d946
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2024-12-16 23:19:54 +0530

    Learn about Encapsulation in Java Programming

---
+++ EEI-3262-JAVA (OOP)/OOP/Encapsulation/EncapsulationDemo.java
@@ -0,0 +1,25 @@
+/*
+  Encapsulation in Java
+
+  Encapsulation is one of the key OOP concepts that restricts access to some of the object's data and ensures that the internal state of an object is manipulated only through well-defined methods. It is ahcieved using private fields and providing public getters and setters.
+*/
+
+
+/*
+  Theories Behind Encapsulation
+
+  1. Purpose:
+      * Protect data from unintended interface and misuse.
+      * Maintain control over data by exposing only what is necessary through methods.
+
+  2. How to Achive Encapsulation:
+      * Declare fields (attributes) as private.
+      * Provide getter and setter methods to access and modify these fields.
+      * Add validation logic in the setters to ensure data integrity.
+  
+  3. Benifits:
+      * Data Hiding: Internal details are hidden, providing only necessary information.
+      * Increased Security: Prevents unauthorized access.
+      * Reusability: Well-encapsulated classes can be reused and modified easily.   
+
+*/
