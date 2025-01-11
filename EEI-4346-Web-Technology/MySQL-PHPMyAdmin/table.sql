02550682ba7a134b8162e6dbd3ae6e6a5abce455
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-01-11 23:47:37 +0530

    'Updated

--- EEI-4346-Web-Technology/MySQL-PHPMyAdmin/table.sql
+++ EEI-4346-Web-Technology/MySQL-PHPMyAdmin/table.sql
@@ -7,3 +7,24 @@ CREATE TABLE `final_practice` (
     `st_tele` DECIMAL(10) NOT NULL,
     PRIMARY KEY (`st_reg_no`)
 ) ENGINE = InnoDB;
+
+
+CREATE TABLE course (
+    c_code VARCHAR(7) PRIMARY KEY,
+    c_title VARCHAR(40),
+    c_credit INT(1),
+    c_level INT(1)
+);
+
+
+CREATE TABLE marks (
+    st_reg_no INT(4),
+    c_code VARCHAR(45),
+    reg_year DATE,
+    marks INT(3),
+    PRIMARY KEY (st_reg_no, c_code),
+    FOREIGN KEY (st_reg_no) REFERENCES students(st_reg_no),
+    FOREIGN KEY (c_code) REFERENCES course(c_code)
+);
+
+
