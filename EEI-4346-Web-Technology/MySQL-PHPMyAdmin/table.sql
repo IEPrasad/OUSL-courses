618b5e3a7dba8d1a5f0e3067d4bfa5174cb3817c
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-01-11 23:49:47 +0530

    full of code for creating database and then the tables

--- EEI-4346-Web-Technology/MySQL-PHPMyAdmin/table.sql
+++ EEI-4346-Web-Technology/MySQL-PHPMyAdmin/table.sql
@@ -1,14 +1,18 @@
-CREATE TABLE `final_practice` (
-    `st_reg_no` INT NOT NULL,
-    `st_name` VARCHAR(45) NOT NULL,
-    `st_dob` DATE NOT NULL,
-    `st_gender` CHAR(1) NOT NULL,
-    `st_address` VARCHAR(60) NOT NULL,
-    `st_tele` DECIMAL(10) NOT NULL,
-    PRIMARY KEY (`st_reg_no`)
-) ENGINE = InnoDB;
+CREATE DATABASE studentinfo;
 
+USE studentinfo;
 
+-- Create "student" table
+CREATE TABLE student (
+    st_reg_no INT(4) PRIMARY KEY,
+    st_name VARCHAR(45) NOT NULL,
+    st_dob DATE NOT NULL,
+    st_gender CHAR(1) NOT NULL,
+    st_address VARCHAR(60) NOT NULL,
+    st_tele DECIMAL(10) NOT NULL
+);
+
+-- Create "course" table
 CREATE TABLE course (
     c_code VARCHAR(7) PRIMARY KEY,
     c_title VARCHAR(40),
@@ -16,15 +20,13 @@ CREATE TABLE course (
     c_level INT(1)
 );
 
-
+-- Create "marks" table
 CREATE TABLE marks (
     st_reg_no INT(4),
     c_code VARCHAR(45),
     reg_year DATE,
     marks INT(3),
     PRIMARY KEY (st_reg_no, c_code),
-    FOREIGN KEY (st_reg_no) REFERENCES students(st_reg_no),
+    FOREIGN KEY (st_reg_no) REFERENCES student(st_reg_no),
     FOREIGN KEY (c_code) REFERENCES course(c_code)
 );
-
-
