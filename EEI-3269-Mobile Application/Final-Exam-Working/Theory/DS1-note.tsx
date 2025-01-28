90c441cd49c57608b7c5bfb77d5acb62202df622
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-01-28 15:32:20 +0530

    DS 1 summary

---
+++ EEI-3269-Mobile Application/Final-Exam-Working/Theory/DS1-note.tsx
@@ -0,0 +1,91 @@
+UNIT 1 - Introduction to Mobile Applications 
+
+- What s Mobile Application?
+    ** A mobile application, most commonly reffered to as an app,
+    is a type of application software designed to run on a mobile 
+    device, such as a smartphone or tablet computer.
+
+---
+
+Key Drivers for Mobile Applications 
+
+  * Smartphone Penetration 
+  * Technological Advancements 
+  * User Experience 
+  * Business Digitalize & E-commerce Growth
+  * Social Media and Communication 
+  * Health and Fitness Trends 
+
+---
+
+key Domain Area of Mobile Apps 
+
+  * Gaming
+  * Music & Entertainment 
+  * Health & Fitness 
+  * Social Networking 
+  * Travel & Hospitality 
+  * Learning & Education 
+
+--- 
+
+Attributes of Mobile applications 
+
+  * Ubiquity 
+  * User friendliness 
+  * Location awareness 
+  * Minimalistic 
+  * High Security 
+
+---
+
+Key Challenges in Mobile App Development: What you need to know 
+
+  - Utility of Mobile App 
+  - Types of Apps 
+  - Principles 
+  - Mobile Users 
+
+  ------
+
+* Learning Outcomes:
+    Identify popular mobile application platforms.
+    Choose the right platform for specific requirements.
+    Prepare for building mobile applications.
+
+* Attributes of Mobile Applications:
+    Ubiquity, user-friendliness, location awareness, high security, and regular updates.
+
+---
+
+Types of Mobile Applications:
+
+  - Native Apss: Built for specific (e.g., iOS, Android).
+  - Cross-Platform Apps: Single codebase for multiple platforms (e.g., React Native).
+  - Hybrid Apps: Web apps in native shell.
+  - Progressive Web Apps (PWAs): Web apps with app-like CSSFontFeatureValuesRule.
+
+---
+
+Mobile App Architecture:
+  - Presentation Layer: UI/UX design.
+  - Business Logic Layer: Process user inputs and data.
+  - Data Access Layer: Manages data transactions.
+  - Backend Services Layer: Handles server-side logic and APIs.
+
+Design Principles: 
+  ** Focus on user experience, consistant design, intutive navigation, and effective communication.
+
+---
+
+
+
+
+
+
+
+
+
+
+
+
