a74f60466ce14576015181698c295710ff792cd9
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-01-28 15:49:44 +0530

    Updated

--- EEI-3269-Mobile Application/Final-Exam-Working/Theory/DS2-note.tsx
+++ EEI-3269-Mobile Application/Final-Exam-Working/Theory/DS2-note.tsx
@@ -31,3 +31,31 @@ Android OS
     - High devicew fragmentation 
     - Security vulnerabilities 
   
+----
+
+iOS 
+
+** Architecture: Built on a Unix core, includes Core OS, Core 
+Services,  Media Layer, and Cocoa Touch 
+
+** Advantages:
+  - Excellent UI 
+  - Strong security features (e.g., Touch ID, encryption)
+  
+** Disadvantages 
+  - Review process for app publishing 
+  - Higher costs for apps 
+
+** Key Differences Between Android and iOS
+  - App Development: Android uses Java/Kotlin, while iOS uses Objective-C/ Swift 
+  - User Interface: iOS has a more controlled environment, while 
+  Android faces more security risks due to its open nature 
+  
+Important Concepts 
+
+  * Memory Management: Critical for efficient operation, involves allocation and deallocation of memory
+
+  * Process Management: Involves creation, scheduling, and termination of process. 
+
+    ----
+    
