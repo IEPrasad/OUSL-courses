000b58a840c9e9a60fbc088fd3865bef4ec6c3ad
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-01-28 15:41:31 +0530

    DS 2 note\ not completed

---
+++ EEI-3269-Mobile Application/Final-Exam-Working/Theory/DS2-note.tsx
@@ -0,0 +1,33 @@
+DS -2 
+
+Functions of an OS:
+
+  ** Peripheral Device Management 
+  ** Data File Mangement 
+  ** Memory Management 
+  ** Process Management 
+
+Types of Mobile Operating Systems 
+
+  ** Popular Mobile OS:
+    Android: Open-source, based on Linux, allows for a unified
+    application development approach. 
+
+    iOS: Developed by Apple, known for its security and fluid
+    user interface.
+
+    Others: Windows Phone, BlackBerry OS, Symbian, EventCounts. etc. 
+
+Android OS 
+
+  ** Architecture: Comprises layers including the infrastructure, Hardware, OS, and Application layers
+
+  ** Advantages: 
+    - Open-source 
+    - Large developer community 
+    - Rich development environment 
+
+  ** Disadvantages: 
+    - High devicew fragmentation 
+    - Security vulnerabilities 
+  
