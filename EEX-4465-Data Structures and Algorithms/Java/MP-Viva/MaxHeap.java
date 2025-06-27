fe35baba2e2a3e34d8907145b0b0f1012fe22e61
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2025-06-27 23:53:53 +0530

    Create MaxHeap.java

---
+++ EEX-4465-Data Structures and Algorithms/Java/MP-Viva/MaxHeap.java
@@ -0,0 +1,55 @@
+class Emergency {
+    String type;
+    String location;
+    int severity;
+    int timeReported;
+
+    Emergency(String type, String location, int severity, int timeReported) {
+        this.type = type;
+        this.location = location;
+        this.severity = severity;
+        this.timeReported = timeReported;
+    }
+}
+
+class MaxHeap {
+    private List<Emergency> heap = new ArrayList<>();
+
+    private int compare(Emergency a, Emergency b) {
+        if (a.severity != b.severity)
+            return Integer.compare(b.severity, a.severity);
+        return Integer.compare(a.timeReported, b.timeReported);
+    }
+
+    public void insert(Emergency e) {
+        heap.add(e);
+        int i = heap.size() - 1;
+        while (i > 0 && compare(heap.get(i), heap.get((i - 1) / 2)) < 0) {
+            Collections.swap(heap, i, (i - 1) / 2);
+            i = (i - 1) / 2;
+        }
+    }
+
+    public Emergency extractMax() {
+        if (heap.isEmpty()) return null;
+        Emergency root = heap.get(0);
+        Emergency last = heap.remove(heap.size() - 1);
+        if (!heap.isEmpty()) {
+            heap.set(0, last);
+            heapify(0);
+        }
+        return root;
+    }
+
+    private void heapify(int i) {
+        int left = 2*i + 1, right = 2*i + 2, largest = i;
+        if (left < heap.size() && compare(heap.get(left), heap.get(largest)) < 0)
+            largest = left;
+        if (right < heap.size() && compare(heap.get(right), heap.get(largest)) < 0)
+            largest = right;
+        if (largest != i) {
+            Collections.swap(heap, i, largest);
+            heapify(largest);
+        }
+    }
+}
